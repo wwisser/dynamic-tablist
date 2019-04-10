758b90bbb7a48837a3fb9d7c2d483e39251a66cf
Wende2k <wisserwendelin@gmail.com>
2019-04-10 16:20:46 +0200

    Added bukkit dependency, created DynamicTablistPlugin

---
+++ plugin/src/main/java/de/wende2k/dynamictablist/DynamicTablistPlugin.java
@@ -0,0 +1,17 @@
+package de.wende2k.dynamictablist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DynamicTablistPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
